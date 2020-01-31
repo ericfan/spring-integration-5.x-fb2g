8d7069c1569a5403c44ebc8873c5835a497dccae
lei.fan <lei.fan@openjawtech.com>
2020-01-31 15:26:36 +0800

    Add sleep feature to test aggregator

--- src/main/java/com/example/fb2g/service/IntService.java
+++ src/main/java/com/example/fb2g/service/IntService.java
@@ -1,7 +1,6 @@
 package com.example.fb2g.service;
 
 import com.example.fb2g.bean.SampleBean;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +8,9 @@ import java.time.LocalDate;
 
 @Service
 public class IntService {
-    public SampleBean getEmp(Integer i) {
+    public SampleBean getEmp(Integer i) throws InterruptedException {
         SampleBean bean = new SampleBean();
+        Thread.sleep(3000);
         if (NumberUtils.compare(10, i) > 0) {
             bean.setAge(i);
             bean.setName("int");
