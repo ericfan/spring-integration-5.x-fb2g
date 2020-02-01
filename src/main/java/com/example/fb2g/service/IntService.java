1c9f12d86964c81a74a088d16f9157ec3601d6fd
lei.fan <lei.fan@openjawtech.com>
2020-02-01 10:36:55 +0800

    Add thread logging

--- src/main/java/com/example/fb2g/service/IntService.java
+++ src/main/java/com/example/fb2g/service/IntService.java
@@ -3,14 +3,18 @@ package com.example.fb2g.service;
 import com.example.fb2g.bean.IntBean;
 import com.example.fb2g.bean.SampleBean;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IntService {
 
+    Logger logger = LoggerFactory.getLogger(IntService.class);
+
     public IntBean getEmp(Integer i) throws InterruptedException {
         IntBean bean = new IntBean();
-        Thread.sleep(3000);
+        logger.info("*********ThreadId is {}, ThreadName is {}", Thread.currentThread().getId(), Thread.currentThread().getName());
         if (NumberUtils.compare(10, i) > 0) {
             bean.setReqNum(i);
             bean.setResNum(i + 10);
--- src/main/java/com/example/fb2g/service/StringService.java
+++ src/main/java/com/example/fb2g/service/StringService.java
@@ -3,6 +3,8 @@ package com.example.fb2g.service;
 import com.example.fb2g.bean.SampleBean;
 import com.example.fb2g.bean.StringBean;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,8 +12,11 @@ import java.time.LocalDate;
 @Service
 public class StringService {
 
+    Logger logger = LoggerFactory.getLogger(StringService.class);
+
     public StringBean getEmp(String str){
         StringBean bean = new StringBean();
+        logger.info("*********ThreadId is {}, ThreadName is {}", Thread.currentThread().getId(), Thread.currentThread().getName());
         if(StringUtils.isAlpha(str)){
             bean.setReqStr(str);
             bean.setResStr(StringUtils.upperCase(str));
