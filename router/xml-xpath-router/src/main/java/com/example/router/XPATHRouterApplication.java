b6cbafc6c11fd0b3fd3083c172fc3baf210cc7ce
lei.fan <lei.fan@openjawtech.com>
2020-02-07 15:42:49 +0800

    SI-10 Initial xpath router code

---
+++ router/xml-xpath-router/src/main/java/com/example/router/XPATHRouterApplication.java
@@ -0,0 +1,13 @@
+package com.example.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class XPATHRouterApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(XPATHRouterApplication.class, args);
+    }
+}
