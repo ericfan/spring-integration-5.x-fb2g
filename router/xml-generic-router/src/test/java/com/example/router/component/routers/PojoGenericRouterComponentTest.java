95634de3dc092dec5fbc10980e4e0b5868b0c705
lei.fan <lei.fan@openjawtech.com>
2020-02-07 14:16:02 +0800

    SI-9 Add unit test and component test code

---
+++ router/xml-generic-router/src/test/java/com/example/router/component/routers/PojoGenericRouterComponentTest.java
@@ -0,0 +1,54 @@
+package com.example.router.component.routers;
+
+import com.example.router.service.DefaultService;
+import com.example.router.service.RouteService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.integration.test.support.AbstractRequestResponseScenarioTests;
+import org.springframework.integration.test.support.PayloadValidator;
+import org.springframework.integration.test.support.RequestResponseScenario;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ContextConfiguration
+@RunWith(SpringRunner.class)
+public class PojoGenericRouterComponentTest {
+
+    @Autowired
+    private MessageChannel inChannel;
+
+    @Autowired
+    private MessageChannel defaultChannel;
+
+    @MockBean(name = "defaultService")
+    private DefaultService defaultService;
+
+    @MockBean(name = "routeService")
+    private RouteService routeService;
+
+    @Test
+    public void testDefaultChannel() {
+        Message message = MessageBuilder.withPayload("DEFAULT").build();
+        inChannel.send(message);
+        Mockito.verify(defaultService, Mockito.times(1)).write(anyString());
+    }
+
+    @Test
+    public void testRouterChannel(){
+        Message message = MessageBuilder.withPayload("REST").build();
+        inChannel.send(message);
+        Mockito.verify(routeService, Mockito.times(1)).write(anyString());
+    }
+}
--- router/xml-generic-router/src/test/java/com/example/router/unit/routers/PojoGenericRouterUnitTest.java
+++ router/xml-generic-router/src/test/java/com/example/router/unit/routers/PojoGenericRouterUnitTest.java
@@ -1,10 +1,28 @@
 package com.example.router.unit.routers;
 
+import com.example.router.routers.PojoGenericRouter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * {@link com.example.router.routers.PojoGenericRouter} unit test case.
+ * @author lei.fan
+ */
+@RunWith(MockitoJUnitRunner.class)
 public class PojoGenericRouterUnitTest {
 
-    public void testRouteChannel(){
+    private PojoGenericRouter genericRouter = new PojoGenericRouter();
 
+    @Test
+    public void testRouteChannel(){
+        assertEquals("routeChannel", genericRouter.route("REST"));
+    }
+
+    @Test
+    public void testDefaultChannel(){
+        assertEquals("defaultChannel", genericRouter.route("DEFAULT"));
     }
 }
